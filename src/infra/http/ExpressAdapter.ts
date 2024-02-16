d2683ce95311a7a4ab7b0bd880f8e176c9ac3595
tarique vieira ramos <tariquevieira@gmail.com>
2024-02-16 20:56:20 -0300

    feat: adding routes

---
+++ src/infra/http/ExpressAdapter.ts
@@ -0,0 +1,22 @@
+import express, { Request, Response } from "express";
+import Http from "./Http";
+
+export default class ExpressAdapter implements Http {
+
+    private app: any;
+
+    constructor() {
+        this.app = express();
+    }
+
+    route(method: string, url: string, callback: Function): void {
+       this.app[method](url, async function (req: Request, res: Response) {
+        const output = await callback(req.params, req.body);
+        res.json(output);
+       });
+    }
+    listen(port: number): void {
+       this.app.listen(port);
+    }
+
+}
--- src/main.ts
+++ src/main.ts
@@ -1,37 +1,13 @@
-import express from "express";
-import BoardService from "./service/BoardService";
-import ColumnService from "./service/ColumnService";
-import CardService from "./service/CardService";
-import PgPromiseConnection from "./infra/database/PgPromiseConnection";
-import BoardRepositoryDatabase from "./infra/repository/BoardRepositoryDatabase";
-import ColumnRepositoryDatabase from "./infra/repository/ColumnRepositoryDatabase";
-import CardRepositoryDatabase from "./infra/repository/CardRepositoryDataBase";
 
-const app = express();
+import BoardController from "./infra/controller/BoardController";
+import PgPromiseConnection from "./infra/database/PgPromiseConnection";
+import ExpressAdapter from "./infra/http/ExpressAdapter";
+
 
 const connection = new PgPromiseConnection();
-const boardRepository = new BoardRepositoryDatabase(connection);
-const columnRepository = new ColumnRepositoryDatabase(connection);
-const cardRepository = new CardRepositoryDatabase(connection);
+const http = new ExpressAdapter();
 
-app.get('/boards', async (req, res) => {
-    const boardService = new BoardService(boardRepository);
-    const boards = await boardService.getBoards();
-    res.json(boards);
-});
+new BoardController(http, connection);
+http.listen(3000)
 
-app.get('/boards/:idBoard/columns', async (req, res) => {
-    
-    const columnService = new ColumnService(columnRepository);
-    const columns = await columnService.getColumns(parseInt(req.params.idBoard));
-    res.json(columns);
-});
 
-app.get('/boards/:idBoard/columns/:idColumn/cards', async (req, res) => {
-    
-    const cardService = new CardService(cardRepository);
-    const cards = await cardService.getCards(parseInt(req.params.idColumn));
-    res.json(cards);
-});
-
-app.listen(3000);
